cea6e5f87bdf2089033d76acee57051b5db7dbb1
Uyi Fred Adun <fredadun@hotmail.com>
2025-03-09 14:20:27 +0000

    Delete scripts\git-aliases.sh

--- scripts\git-aliases.sh
+++
@@ -1,6 +0,0 @@
-# Add these to your .bashrc or .zshrc
-alias lp-init='./scripts/init-repo.sh'
-alias lp-dev='./scripts/checkout-dev.sh'
-alias lp-commit='./scripts/commit-changes.sh'
-alias lp-feature='./scripts/feature-branch.sh'
-alias lp-update='./scripts/update-code.sh'
