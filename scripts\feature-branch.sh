d3b17f1c6b69d81d4bc0fb4f09ed7d0a5ddf4aca
Uyi Fred Adun <fredadun@hotmail.com>
2025-03-09 13:59:03 +0000

    Update scripts\feature-branch.sh

---
+++ scripts\feature-branch.sh
@@ -0,0 +1,9 @@
+# Create and checkout new feature branch
+git checkout -b feature/initial-setup development
+
+# Make your changes and commit
+git add .
+git commit -m "CP-123: [Feature description]"
+
+# Push feature branch to remote
+git push -u origin feature/initial-setup
