3ae2ef35a65be15f5886687c990dee69bb4ea223
Uyi Fred Adun <fredadun@hotmail.com>
2025-03-09 13:59:10 +0000

    Update scripts\git-aliases.sh

---
+++ scripts\git-aliases.sh
@@ -0,0 +1,6 @@
+# Add these to your .bashrc or .zshrc
+alias lp-init='./scripts/init-repo.sh'
+alias lp-dev='./scripts/checkout-dev.sh'
+alias lp-commit='./scripts/commit-changes.sh'
+alias lp-feature='./scripts/feature-branch.sh'
+alias lp-update='./scripts/update-code.sh'
