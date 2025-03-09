0111b1d026ca23e3c7e62963fc81495a64975a87
Uyi Fred Adun <fredadun@hotmail.com>
2025-03-09 14:23:31 +0000

    Update scripts/update-code.sh

---
+++ scripts/update-code.sh
@@ -0,0 +1,8 @@
+# Make sure you're on development and up to date
+git checkout development
+git pull origin development
+
+# Make changes, then commit and push
+git add .
+git commit -m "CP-123: Update [description]"
+git push origin development
